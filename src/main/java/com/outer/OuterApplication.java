992ebb84a14042a65baf23b9c39ac83011fccf4b
v-kecongcong <v-kecongcong@huazhu.com>
2018-08-08 18:37:28 +0800

    dubbo配置

--- src/main/java/com/outer/OuterApplication.java
+++ src/main/java/com/outer/OuterApplication.java
@@ -3,10 +3,11 @@ package com.outer;
 import com.alibaba.dubbo.spring.boot.annotation.EnableDubboConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin//允许跨越访问
-@SpringBootApplication//springBoot项目入口
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 @EnableDubboConfiguration
 public class OuterApplication {
 
